845e44d8c1378fccf1ec031e15b827de177fc378
bananaWeb <a89.volkov@gmail.com>
2018-02-05 20:56:44 +0300

    slozno-neponyanto

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
 $(document).ready(function() {
-	
-	$('.js-sayThat').click(function() {
-		showBubble($('.js-text1').val() + $('.js-text2').val());
+
+
+	$('.js-plus').click(function() {
+			showBubble(Number(getFirstWord()) + Number(getSecondWord()));
 	});
+	$('.js-minus').click(function() {
+			showBubble(Number(getFirstWord()) - Number(getSecondWord()));
+	});
+	$('.js-multiply').click(function() {
+		showBubble(Number(getFirstWord()) * Number(getSecondWord()));
+	});
+	$('.js-divide').click(function() {
+		if (checkInputData().isNumber) {
+		showBubble(Number(getFirstWord()) / Number(getSecondWord()));	
+		} else {
+			if (checkInputData().isEmpty) {
+				showBubble('Пустоши');
+
+			} else showBubble(getFirstWord() + ' ' + getSecondWord());
+		}
+		
+	});
+
+	// ==========================================
+	// ==========================================
+
+	function checkInputData () {
+		// проверить число ли это
+		// не ничего 
+		// не равно 0
+
+		if ($.isNumeric(getFirstWord()) && $.isNumeric(getSecondWord())) {
+			return {
+				isNumber: true
+			};
+		} else if (getFirstWord() === '' && getSecondWord() === '') {
+			return {
+				isNumber: false,
+				isEmpty: true
+			};
+
+		}else {
+			return {
+				isNumber: false,
+				isEmpty: false
+			}
+		}
+	};
+
+	function getFirstWord () {
+		return $('.js-text1').val();
+	}
+	function getSecondWord () {
+		return $('.js-text2').val();
+	}
 
 	function showBubble(text) {
 		$('.bubble').text(text);
 		$('.bubble').addClass('-visible');
+
 		setTimeout(function() {				
 			$('.bubble').removeClass('-visible');
 		}, 2000);
